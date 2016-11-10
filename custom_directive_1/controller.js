d48df7dae403e67f710aaa2be3ffd3757fdc9096
jia <edison7476@hotmail.com>
2016-11-09 21:45:55 -0800

    AngularJS custom directive example

--- custom_directive_1/controller.js
+++ custom_directive_1/controller.js
@@ -2,4 +2,14 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('myController', function ($scope){
   $scope.h = "hello";
+
+  $scope.players = [
+      {name:"Allen Iverson", img:"iverson.jpeg",careerAvgPts:26.7},
+      {name:"Kyrie Irving", img:"irving.jpeg", careerAvgPts:20.8 },
+      {name:"Stephen Curry", img:"curry.jpeg", careerAvgPts:22.5 },
+      {name:"Kobe Bryat", img:"kobe.jpeg", careerAvgPts:25.0},
+      {name:"Lebron James", img:"lebron.jpeg", careerAvgPts:27.2},
+      {name:"Chris Paul", img:"paul.jpeg", careerAvgPts:18.8},
+      {name:"Dwyane Wade", img:"wade.jpeg", careerAvgPts:23.8}
+  ];
 });
