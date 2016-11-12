be2396774d17aa46a3fbc3bd9a5f0ebf70339b75
jia <edison7476@hotmail.com>
2016-11-11 17:08:26 -0800

    AngularJS custom directive example

--- custom_directive_1/controller.js
+++ custom_directive_1/controller.js
@@ -1,31 +1,27 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('myController', function ($scope){
+myApp.controller('myController', function ($scope, $http){
 
   $scope.condition = 'name';
 
   $scope.sortBy = function (condition){
     $scope.condition = condition;
-    console.log($scope.condition );
+    // console.log($scope.condition );
   };
 
-  $scope.players = [
-      {name:"Allen Iverson", img:"iverson.jpeg",careerAvgPts:26.7},
-      {name:"Kyrie Irving", img:"irving.jpeg", careerAvgPts:20.8 },
-      {name:"Stephen Curry", img:"curry.jpeg", careerAvgPts:22.5 },
-      {name:"Kobe Bryat", img:"kobe.jpeg", careerAvgPts:25.0},
-      {name:"Lebron James", img:"lebron.jpeg", careerAvgPts:27.2},
-      {name:"Chris Paul", img:"paul.jpeg", careerAvgPts:18.8},
-      {name:"Dwyane Wade", img:"wade.jpeg", careerAvgPts:23.8}
-  ];
+  $http.get('data/players.json').then(function (response){
+    $scope.players = response.data;
+  });
+
 });
 
 myApp.directive('playerDirective', function (){
   return {
-    restric:'A',
+    restric:'AE',
     scope:{
       player:"="
     },
+    replace:true,
     templateUrl: "playerDirective.html"
   };
 });
