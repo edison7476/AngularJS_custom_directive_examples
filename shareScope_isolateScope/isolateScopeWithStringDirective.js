4ba844238468050ec502df3da7647782730d77b9
jia <edison7476@hotmail.com>
2016-11-18 11:49:28 -0800

    Added an exampler of one way data binding using @

---
+++ shareScope_isolateScope/isolateScopeWithStringDirective.js
@@ -0,0 +1,16 @@
+(function () {
+  var app = angular.module('app');
+
+  app.directive('isolateScopeWithStringDirective',function(){
+    return {
+      // Difine scope name and ppt for isolateScopeWithStringDirective
+      // and use them as attibutes in html page
+      scope: {
+        name : '@', // this is one way binning to bind data from controller to the directive of isolateScopeWithStringDirective
+        ppt: '@'
+      },
+      // setup a template to display scope in html page
+      template: '<p> Name: {{ name }} </p> <p>Carrer Average PPT: {{ ppt }} </p>'
+    };
+  });
+}());
