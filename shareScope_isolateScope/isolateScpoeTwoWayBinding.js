79a9ae7ee87cb471e61f97a97bd9492f7280bd48
jia <edison7476@hotmail.com>
2016-11-19 10:10:38 -0800

    Added an example of a two way data binding directive using '='

--- shareScope_isolateScope/isolateScpoeTwoWayBinding.js
+++ shareScope_isolateScope/isolateScpoeTwoWayBinding.js
@@ -7,7 +7,8 @@
       },
       template: '<p> Name: {{ datasource.name }} </p> <p>Carrer Average PPG: {{ datasource.ppt }} </p>' +
                 '<br />' +
-                '<button ng-click=" datasource.name =\'Kobe Bryant\'  " > Change </button>'
+                '<button ng-click=" datasource.name =\'Kobe Bryant\'  " > Change </button> '+
+                '<p style="color:red">* Since it is a two-way binding directive, if we change the value of scope in this directive, that will also change the value of $scope in the controller</p>'
     };
   });
 
